388bc39b20d3698c59262ced86f981be604a451b
Daria Motronenko <150202671+Dariamotronenko@users.noreply.github.com>
2025-05-06 10:34:47 +0300

    Update settings.py

--- kfxkobin/kfxkobin/settings.py
+++ kfxkobin/kfxkobin/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gyp6ssio3%tkdfx8$$j_6$l4a41pxa$cd2iy7wm%4r^nxop(om'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
